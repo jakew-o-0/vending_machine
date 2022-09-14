83387745caa99b219b1bfeae8696297bd294ecd2
jakew-o-0 <jakewbailey007@gmail.com>
2022-09-14 13:46:31 +0100

    first commit gui half built and gui only

---
+++ gui.py
@@ -0,0 +1,86 @@
+import tkinter
+
+class gui:
+    def __init__(self, root):
+        self.root = root
+        self.root.resizable(False,  False)
+        self.main()
+
+    def main(self):
+        ########: bulds menu bar with cascades
+        menuBar = tkinter.Menu(self.root)
+
+        profile = tkinter.Menu(menuBar, tearoff=0)
+        profile.add_command(label="Account", command=self.account_window)
+        profile.add_command(label="Basket", command=self.basket_window)
+        profile.add_separator()
+        profile.add_command(label="Login", command=self.logon_window)
+        profile.add_command(label="Quit", command=self.root.quit)
+
+        menuBar.add_cascade(label="Profile", menu=profile)
+        self.root.config(menu=menuBar)
+    
+
+        ########: frames with widgets specific searching of the database
+        frame = tkinter.Frame(self.root, padx=5,pady=5)
+        frame.pack(padx=10,pady=10)
+
+        searchLib = tkinter.Label(frame, text="If you know what you want...",font=("TkDefaultFont", 8))
+        searchLib1 = tkinter.Label(frame, text="Search!", font=("TkDefaultFont", 20))
+        searchEntry = tkinter.Entry(frame, width=15)
+        searchButton = tkinter.Button(frame, text="Enter", command=self.search_window)
+
+        searchLib.grid(column=0, row=0, columnspan=2, sticky='w')
+        searchLib1.grid(column=0, columnspan=2, row=1)
+        searchEntry.grid(column=0, row=2, padx=2)
+        searchButton.grid(column=1, row=2)
+
+
+        ########: second frame with widgets random and all searching of the data base
+        frame2 = tkinter.Frame(self.root, padx=5, pady=5)
+        frame2.pack(padx=10, pady=10)
+
+        searchLib2 = tkinter.Label(frame2, text="Not sure what to get?", font=("TkDefaultFont", 8))
+        searchLib3 = tkinter.Label(frame2, text="Try these!", font=("TkDefaultFont", 20))
+        searchAllBtn = tkinter.Button(frame2, text="Browse all", command=self.search_window)
+        searchRandBtn = tkinter.Button(frame2, text="Random", command=self.search_window)
+
+        searchLib2.grid(column=0, row=0, columnspan=2, sticky='w')
+        searchLib3.grid(column=0, row=1, columnspan=2)
+        searchAllBtn.grid(column=0, row=2)
+        searchRandBtn.grid(column=1, row=2)
+
+    def search_window(self):
+        dataWindow = tkinter.Toplevel(self.root)
+        noneFound = tkinter.Label(dataWindow, text="Item not found :(")
+        noneFound.pack()
+
+    def account_window(self):
+        acountWindow = tkinter.Toplevel(self.root)
+    
+    def basket_window(self):
+        basketWindow = tkinter.Toplevel(self.root)
+    
+    def logon_window(self):
+        logonWindow = tkinter.Toplevel(self.root)
+        logonWindow.resizable(False, False)
+
+        logonLab = tkinter.Label(logonWindow, text="Login", font=("TkDefaultFont", 15))
+        logonLab1 = tkinter.Label(logonWindow, text="Username")
+        logonLab2 = tkinter.Label(logonWindow, text="Password")
+        logonEntry = tkinter.Entry(logonWindow)
+        logonEntry1 = tkinter.Entry(logonWindow, show='*')
+        logonBtn = tkinter.Button(logonWindow, text="Enter")
+
+        logonLab.grid(column=0, row=0, columnspan=2)
+        logonLab1.grid(column=0, row=1)
+        logonLab2.grid(column=0, row=2)
+        logonEntry.grid(column=1, row=1)
+        logonEntry1.grid(column=1, row=2)
+        logonBtn.grid(column=0, row=3, columnspan=2)
+
+
+if(__name__ == "__main__"):
+    tk = tkinter.Tk()
+    gui = gui(tk)
+    tk.mainloop()
